f39161b6bd407a77107d9a9103bffae4e3abdd92
COPY0096 <73365279+COPY0096@users.noreply.github.com>
2021-11-30 16:05:45 -0300

    Empezamos proyecto

--- src/proyecto/gym/ProyectoGYM.java
+++ src/proyecto/gym/ProyectoGYM.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.gym;
 
-/**
- *
- * @author jerem
- */
 public class ProyectoGYM {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+    }//final main
     
-}
+}//final clase ProyectoGYM
