40a06f403e309c3e6091593a59427ff2a23a0d13
Jhoan <Jhoan@192.168.0.107>
2021-12-02 22:19:55 -0400

    Actualice el Jframe de Inicio de Seccion

--- src/proyecto/gym/InicioSesion.java
+++ src/proyecto/gym/InicioSesion.java
@@ -18,12 +18,19 @@ public class InicioSesion extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jButtonSalir = new javax.swing.JButton();
-        jLabelicono = new javax.swing.JLabel();
-        jLabelUsurario = new javax.swing.JLabel();
-        jTextFieldUsuario = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
+        FieldUsuario = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
         jPassword = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        Ingresar = new javax.swing.JButton();
+        NivelAcceso = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        NombreUsuario = new javax.swing.JTextField();
+        Apellidos = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
         jLabelFondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -32,49 +39,87 @@ public class InicioSesion extends javax.swing.JFrame {
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButtonSalir.setBackground(new java.awt.Color(102, 153, 255));
+        jButtonSalir.setBackground(new java.awt.Color(255, 255, 255));
         jButtonSalir.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jButtonSalir.setForeground(new java.awt.Color(0, 0, 204));
+        jButtonSalir.setForeground(new java.awt.Color(102, 102, 255));
         jButtonSalir.setText("Salir");
         jButtonSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonSalirActionPerformed(evt);
             }
         });
-        jPanel1.add(jButtonSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 40, -1, -1));
-
-        jLabelicono.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/User.png"))); // NOI18N
-        jPanel1.add(jLabelicono, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, -1, 270));
-
-        jLabelUsurario.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabelUsurario.setText("Usuario:");
-        jPanel1.add(jLabelUsurario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 290, 70, 40));
-
-        jTextFieldUsuario.setBackground(new java.awt.Color(204, 204, 204));
-        jTextFieldUsuario.setForeground(new java.awt.Color(0, 0, 204));
-        jPanel1.add(jTextFieldUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 130, 30));
+        jPanel1.add(jButtonSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, -1, -1));
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabel1.setText("Password:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 360, 80, 30));
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Usuario:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, 80, 30));
+
+        FieldUsuario.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.add(FieldUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 90, 250, 30));
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Password:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 80, 30));
 
         jPassword.setBackground(new java.awt.Color(204, 204, 204));
-        jPassword.setForeground(new java.awt.Color(0, 0, 204));
-        jPanel1.add(jPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 360, 130, 30));
+        jPanel1.add(jPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 240, 30));
 
-        jButton1.setBackground(new java.awt.Color(0, 0, 255));
-        jButton1.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(102, 153, 255));
-        jButton1.setText("Ingresar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Nivel acceso (0,1):");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 190, 130, 30));
+
+        Ingresar.setBackground(new java.awt.Color(255, 255, 255));
+        Ingresar.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        Ingresar.setForeground(new java.awt.Color(102, 153, 255));
+        Ingresar.setText("Ingresar");
+        Ingresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                IngresarActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 420, -1, -1));
+        jPanel1.add(Ingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 400, 110, 40));
 
+        NivelAcceso.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.add(NivelAcceso, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 190, 190, 30));
+
+        jLabel4.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Nombre: ");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 90, 30));
+
+        jLabel5.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Apellidos: ");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 290, 70, 30));
+
+        NombreUsuario.setBackground(new java.awt.Color(204, 204, 204));
+        NombreUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NombreUsuarioActionPerformed(evt);
+            }
+        });
+        jPanel1.add(NombreUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, 250, 30));
+
+        Apellidos.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.add(Apellidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 290, 240, 30));
+
+        jLabel6.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Correo:");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 350, 60, -1));
+
+        jTextField1.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 340, 260, 30));
+
+        jLabelFondo.setBackground(new java.awt.Color(204, 204, 204));
         jLabelFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Fondo.png"))); // NOI18N
-        jPanel1.add(jLabelFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(-60, 0, -1, -1));
+        jPanel1.add(jLabelFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(-30, 0, -1, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 350, 500));
 
@@ -85,9 +130,9 @@ public class InicioSesion extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_jButtonSalirActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void IngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_IngresarActionPerformed
         // TODO add your handling code here:
-        if(jTextFieldUsuario.getText().equals("admin")&& jPassword.getText().equals("1234"))
+        if(FieldUsuario.getText().equals("admin")&& jPassword.getText().equals("1234"))
         {
             setVisible(false);
             new MenuPrincipal().setVisible(true);
@@ -95,7 +140,11 @@ public class InicioSesion extends javax.swing.JFrame {
             
         }
         
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_IngresarActionPerformed
+
+    private void NombreUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NombreUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_NombreUsuarioActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -131,14 +180,21 @@ public class InicioSesion extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JTextField Apellidos;
+    private javax.swing.JTextField FieldUsuario;
+    private javax.swing.JButton Ingresar;
+    private javax.swing.JTextField NivelAcceso;
+    private javax.swing.JTextField NombreUsuario;
     private javax.swing.JButton jButtonSalir;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabelFondo;
-    private javax.swing.JLabel jLabelUsurario;
-    private javax.swing.JLabel jLabelicono;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPassword;
-    private javax.swing.JTextField jTextFieldUsuario;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
